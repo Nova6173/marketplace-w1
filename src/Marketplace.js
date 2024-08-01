811847be08546ed9b7ac0bb9cdf3ee21832b4fa9
Jeanette S <nova777nova@hotmail.com>
2024-08-01 11:47:15 +0200

    changed ads text info

--- src/Marketplace.js
+++ src/Marketplace.js
@@ -6,12 +6,12 @@ import Advertisement from './Advertisement';
 
 const Marketplace = () => {
   const ads = [
-    { id: 1, title: 'Ad 1', description: 'This is the first advertisement.', contact: 'contact1@gmail.com ' }, 
-    { id: 2, title: 'Ad 2', description: 'This is the second advertisement.', contact: 'contact2@gmail.com ' },
-    { id: 3, title: 'Ad 3', description: 'This is the third advertisement.', contact: 'contact3@gmail.com' },
-    { id: 4, title: 'Ad 4', description: 'This is the fourth advertisement.', contact: 'contact4@gmail.com' },
-    { id: 5, title: 'Ad 5', description: 'This is the fifth advertisement.', contact: 'contact5@gmail.com' },
-    { id: 6, title: 'Ad 6', description: 'This is the sixth advertisement.', contact: 'contact6@gmail.com' },
+    { id: 1, title: 'Product 1', description: 'Description', contact: 'contact1@gmail.com ' }, 
+    { id: 2, title: 'Product 2', description: 'Description', contact: 'contact2@gmail.com ' },
+    { id: 3, title: 'Product 3', description: 'Description', contact: 'contact3@gmail.com' },
+    { id: 4, title: 'Product 4', description: 'Description', contact: 'contact4@gmail.com' },
+    { id: 5, title: 'Product 5', description: 'Description', contact: 'contact5@gmail.com' },
+    { id: 6, title: 'Product 6', description: 'Description', contact: 'contact6@gmail.com' },
   ];
 
   return (
